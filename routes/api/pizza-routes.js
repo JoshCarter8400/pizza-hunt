a3548ef77d9b86cc3ef86b7609a608157b7334f5
Josh Carter <joshcarter8400@gmail.com>
2020-12-04 21:58:36 -0500

    added pizza routes

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,11 @@
+const { post, route, get } = require("../html/html-routes");
+
+const router = require("express").Router();
+
+// Set up GET all and POST at /api/pizzas
+router.route("/").get.post();
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router.route("/:id").get().put().delete();
+
+module.exports = router;
